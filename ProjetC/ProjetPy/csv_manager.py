09b4a8971a2f5722dc206b8c556c9d1f32794efc
SikhouDevops <134374963+SikhouDevops@users.noreply.github.com>
2023-05-25 15:56:00 +0200

    Delete ProjetC

--- ProjetC/ProjetPy/csv_manager.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-class CSVManager:
-
-    def read_csv(self, path):
-        with open(path, newline='') as csvfile:
-            csvreader = csv.reader(csvfile, delimiter=';')
-            next(csvreader)  # Skip the header row
-            return [row for row in csvreader]
--- ProjetC/ProjetPy/file_manager.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-    def remove_file(self, path):
-        if os.path.isfile(path):
-            os.remove(path)
-
-    def copy_file(self, source, destination):
-        shutil.copy(source, destination)
